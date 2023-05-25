ad7ec95770cf51caf0b01ade038b82ec3f62bd5f
Chucky.Tsai <64692566+chuckytsai@users.noreply.github.com>
2023-05-25 10:20:44 +0800

    Revert "Merge pull request #1 from chuckytsai/nav20230524"
    This reverts commit f2b32d57693a440a946221094c4ad36c59eefcd0, reversing
    changes made to a9104b1de54f12b9585edf4d38712b0a96e657fb.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
-  <TemplateNav />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import TemplateNav from "./components/TemplateNav.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    TemplateNav,
-  },
-};
+    HelloWorld
+  }
+}
 </script>
 
 <style>
-* {
-  margin: 0;
-  padding: 0;
-}
 #app {
-  background-color: black;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
