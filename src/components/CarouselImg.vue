f80b1d1c0f95b5b9ae3f5bfeeb071c4aab2e20b1
Chucky.Tsai <64692566+chuckytsai@users.noreply.github.com>
2023-06-12 13:58:30 +0800

    feat: 無限輪撥調整只有一個setTimeout(一個視窗)

--- src/components/CarouselImg.vue
+++ src/components/CarouselImg.vue
@@ -3,7 +3,7 @@
     <div class="bannerControl">
       <button type="button" @click="directionControl(false)">
         <img
-          src="../../public/imgs/arrow.png"
+          src="/imgs/arrow.png"
           alt="左按鈕"
           style="transform: rotate(180deg)"
         />
@@ -22,7 +22,7 @@
         </ul>
       </div>
       <button type="button" @click="directionControl(true)">
-        <img src="../../public/imgs/arrow.png" alt="右按鈕" />
+        <img src="/imgs/arrow.png" alt="右按鈕" />
       </button>
     </div>
     <div>
@@ -41,7 +41,7 @@
 </template>
 
 <script>
-import { toRefs, reactive, computed } from "vue";
+import { toRefs, reactive, computed, watch } from "vue";
 
 export default {
   props: ["carouselArry"], // 決定圖片數量清單
@@ -49,20 +49,24 @@ export default {
     const data = reactive({
       index: 0, // 顯示第幾個圖 預設為 0
       speed: "all 0s linear 0s, all 1.5s ease 0s",
-      // speed: "all 0s linear 0s, all 1s ease 0s",
     });
 
+    // dot按鈕亮燈狀態
     const dotActive = computed(() => {
       if (data.index === props.carouselArry.length) {
         return 0;
+      } else if (data.index === -1) {
+        return props.carouselArry.length - 1;
       }
       return data.index;
     });
 
+    // 計算整個圖片清單移動距離
     const distance = computed(() => {
       return data.index * -375 + "px";
     });
 
+    // 圖片清單(起始為api資料最後一張 , 最後一張圖為api資料第一張)
     const list = computed(() => {
       return [
         props.carouselArry[props.carouselArry.length - 1],
@@ -73,49 +77,48 @@ export default {
 
     // 點選dot 變化圖片
     const clickMe = (index) => {
-      data.index = index;
-      // data.distance = (index - (index * 2 - 1)) * 375 + "px";
+      const imgAmt = props.carouselArry;
+      if (imgAmt.length > 1) {
+        data.index = index;
+        if (
+          (data.index === 1 && dotActive.value === 1) ||
+          (data.index === imgAmt.length - 2 &&
+            dotActive.value === imgAmt.length - 2)
+        ) {
+          data.speed = "all 0s linear 0s, all 1.5s ease 0s";
+        }
+      }
     };
 
     // 點選左右邊按鈕 變化圖片以及dot
     // direction布林值(左邊:false, 右邊:true)
     const directionControl = (direction) => {
       const imgAmt = props.carouselArry;
-      // data.speed = "all 0s linear 0s, all 1s ease 0s";
       if (direction === true && data.index < imgAmt.length) {
         clickMe(data.index + 1);
+      } else if (direction === false && data.index >= 0) {
+        clickMe(data.index - 1);
       }
-
-      setTimeout(() => {
-        if(data.index === imgAmt.length){
-          console.log("back");
-        }
-      }, 1000);
-
-      // else if (direction === true && data.index + 1 === imgAmt.length) {
-      //   clickMe(0);
-      // }
-
-      // else if (direction === true && data.index === list.value.length - 1) {
-      // data.imgAmt = [];
-      // data.imgAmt.push(...props.carouselArry, props.carouselArry[0]);
-      // data.index = 0;
-      // data.distance =
-      //   (data.imgAmt.length - 1 - ((data.imgAmt.length - 1) * 2 - 1)) * 375 +
-      //   "px";
-      // setTimeout(() => {
-      //   data.speed = "all 0s linear 0s, all 0s ease 0s";
-      //   data.distance = "375px";
-      //   data.imgAmt = props.carouselArry;
-      // }, 1000);
-      //   clickMe(0);
-      // }
-      // else if (direction === false && data.index !== 0) {
-      //   clickMe(data.index - 1);
-      // } else if (direction === false && data.index === 0) {
-      //   clickMe(props.carouselArry.length - 1);
-      // }
     };
+
+    // 點擊左右或是dot 讓index值變化才執行
+    watch(
+      () => data.index,
+      () => {
+        const imgAmt = props.carouselArry;
+        if (data.index === imgAmt.length && dotActive.value === 0) {
+          setTimeout(() => {
+            data.speed = "all 0s linear 0s, all 0s ease 0s";
+            clickMe(0);
+          }, 1500);
+        } else if (data.index === -1 && dotActive.value === imgAmt.length - 1) {
+          setTimeout(() => {
+            data.speed = "all 0s linear 0s, all 0s ease 0s";
+            clickMe(imgAmt.length - 1);
+          }, 1500);
+        }
+      }
+    );
 
     return {
       props,
@@ -136,6 +139,7 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+
     button {
       width: 45px;
       height: 30px;
@@ -144,10 +148,12 @@ export default {
       z-index: 1;
       margin: 0 -22.5px;
       background-color: transparent;
+
       img {
         opacity: 0.9;
       }
     }
+
     button:active img {
       opacity: 1;
     }
@@ -157,17 +163,20 @@ export default {
     height: 280px;
     position: relative;
     overflow: hidden;
+
     .carouselBanner {
       display: flex;
       position: absolute;
       width: 100vw;
       transform: translateX(-375px);
+
       .imgs {
         list-style: none;
         display: flex;
         justify-content: flex-end;
         align-items: center;
       }
+
       img {
         width: 375px;
         height: 280px;
@@ -180,10 +189,12 @@ export default {
     display: flex;
     justify-content: flex-start;
     padding-left: 1%;
+
     li {
       list-style: none;
       margin: 10px 2%;
     }
+
     button {
       width: 20px;
       height: 20px;
@@ -192,6 +203,7 @@ export default {
       border-radius: 50%;
       color: white;
     }
+
     .active {
       opacity: 1;
     }
